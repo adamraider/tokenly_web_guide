cc65f8d059d41b6ea588ae76b271f7b71587ea26
Adam Raider <adamjraider@gmail.com>
2016-06-29 12:39:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Swapbot HTML/CSS
+==============
+
+## Instructions
+
+```bash
+# Download project and unzip
+cd swapbot_html
+npm install
+gulp serve
+```
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    util = require('gulp-util'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    watch = require('gulp-watch');
+
+var errorHandler = function (error) {
+  console.log(error.message);
+  this.emit("end");
+}
+
+gulp.task('default', ['styles']);
+
+gulp.task('styles', function() {
+  return gulp.src('./src/sass/application.sass')
+    .pipe(plumber({ errorHandler: errorHandler }))
+    .pipe(sass({ indentedSyntax: true }).on('error', util.log))
+    .pipe(concat('application.css'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({ stream:true }))
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  watch('./src/sass/**/*.sass', function(){ gulp.start('styles'); })
+});
